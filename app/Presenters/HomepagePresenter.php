8a110f08285f29c1513edcdf90828e808eb09646
Emanuel Neumann <neumann@stampi.cz>
2023-07-22 16:33:49 +0200

    pig latin translator

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,8 +5,78 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-
+use Nette\Application\UI\Form;
+use stdClass;
 
 final class HomepagePresenter extends BasePresenter
 {
+    /**
+     * @return Form
+     */
+    protected function createComponentPigLatinForm(): Form
+    {
+        $form = new Form();
+
+        $form->setHtmlAttribute('class', 'ajax');
+
+        $form->addTextArea('text', 'Add text')
+            ->setHtmlAttribute('placeholder', 'Add text');
+
+        $form->addSubmit('submit', 'Translate');
+
+        $form->onSuccess[] = [$this, 'pigLatinFormSucceeded'];
+
+        return $form;
+    }
+
+    /**
+     * @param Form $form
+     * @param stdClass $values
+     * @return void
+     */
+    public function pigLatinFormSucceeded(Form $form, stdClass $values): void
+    {
+        $consonant = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z');
+        $vowels = array('a', 'e', 'i', 'o', 'u', 'y');
+        $special = array('?', '.', '!', ',');
+
+        $words = explode(' ', trim(strtolower($values->text)));
+
+        $result = array();
+
+        foreach ($words as $word)
+        {
+            $position = 0;
+            $consonantCluster = '';
+            $specialCharacters = '';
+
+            for ($i = strlen($word) - 1; $i >= 0; $i--){
+                if (in_array($word[$i], $special)){
+                    $specialCharacters .= $word[$i];
+                    $word = substr_replace($word, '', $i, 1);
+                }else break;
+            }
+
+            if (preg_match('/[a-zA-Z]/u', $word)) {
+                if (!in_array($word[0], $vowels)){
+                    foreach (str_split($word) as $char){
+                        if (in_array($char, $consonant) && !in_array($char, $special)){
+                            $consonantCluster .= $char;
+                            $position ++;
+                        } else break;
+                    }
+                }
+
+                $substr = substr($word, $position);
+                if ($substr) $result[] =  $substr . '-' . $consonantCluster . 'ay' . $specialCharacters;
+                else $result[] = $consonantCluster . '-ay' . $specialCharacters;
+
+            } else {
+                $result[] = $word;
+            }
+        }
+
+        $this->template->result = ucfirst(implode(' ', $result));
+        $this->redrawControl();
+    }
 }
--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -16,3 +16,18 @@ import '../scss/main.scss'
 
 /** import bootstrap **/
 import * as bootstrap from 'bootstrap';
+
+function init_resizable_textarea(){
+    $("textarea").each(function () {
+        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;resize:none;");
+    }).on("input", function () {
+        this.style.height = 0;
+        this.style.height = (this.scrollHeight) + "px";
+    });
+}
+
+naja.snippetHandler.addEventListener('afterUpdate', (event) => {
+    init_resizable_textarea();
+});
+
+init_resizable_textarea();
